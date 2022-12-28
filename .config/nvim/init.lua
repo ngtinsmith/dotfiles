6e22ae449813f2890b5f78a521fd8d000f87afb2
ng <ng@192.168.1.8>
2022-12-28 22:30:00 +0800

    nvim: add user/projects modules

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,4 @@
--- # Modules
+-- Core
 
 require('user/plugins')
 require('user/colors')
@@ -6,10 +6,15 @@ require('user/keymaps')
 require('user/core/autocmd')
 require('user/core/options')
 
--- # Dotfiles
+-- Projects
+
+require('user/projects/keymaps').setup()
+require('user/projects/commands')
+
+-- Dotfiles
+--
 -- used for diffview.nvim
 -- TODO: find how to set in bare repo
 
 vim.env.GIT_WORK_TREE = vim.fn.expand('~')
 vim.env.GIT_DIR = vim.fn.expand('~/.cfg')
-
