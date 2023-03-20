11c1534d2b794aba059661d82cb362867d3e1a5c
ng <ng@192.168.1.6>
2023-03-20 13:57:28 +0800

    nvim(packer): add neo-tree and rust-tools plugins
    - auto-session: add pre_save_cmd hooks

--- .config/nvim/lua/user/plugins/packer.lua
+++ .config/nvim/lua/user/plugins/packer.lua
@@ -10,6 +10,14 @@ require('packer').startup(function(use)
                 auto_session_suppress_dirs = { '~/', '~/projects', '~/Downloads', '/' },
             }
         end,
+        pre_save_cmds = {
+            function()
+                print('pre save cmds')
+            end,
+            function()
+                require('neo-tree').close('filesystem')
+            end
+        },
         cwd_change_handling = {
             post_cwd_changed_hook = function()
                 require('lualine').refresh()
@@ -31,6 +39,38 @@ require('packer').startup(function(use)
     }
     use { 'lukas-reineke/indent-blankline.nvim', }
 
+    use {
+        'nvim-neo-tree/neo-tree.nvim',
+        branch = 'v2.x',
+        requires = {
+            'nvim-lua/plenary.nvim',
+            'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+            'MunifTanjim/nui.nvim',
+            {
+                -- only needed if you want to use the commands with "_with_window_picker" suffix
+                's1n7ax/nvim-window-picker',
+                tag = 'v1.*',
+                config = function()
+                    require 'window-picker'.setup({
+                        autoselect_one = true,
+                        include_current = false,
+                        filter_rules = {
+                            -- filter using buffer options
+                            bo = {
+                                -- if the file type is one of following, the window will be ignored
+                                filetype = { 'neo-tree', 'neo-tree-popup', 'notify' },
+
+                                -- if the buffer type is one of following, the window will be ignored
+                                buftype = { 'terminal', 'quickfix' },
+                            },
+                        },
+                        other_win_hl_color = '#e35e4f',
+                    })
+                end,
+            }
+        }
+    }
+
     -- FZF
 
     use { 'ibhagwan/fzf-lua',
@@ -49,6 +89,11 @@ require('packer').startup(function(use)
 
     use 'neovim/nvim-lspconfig'
     use 'folke/neodev.nvim'
+
+    -- Rust
+
+    use 'simrat39/rust-tools.nvim'
+    use 'mfussenegger/nvim-dap'
 
     -- Completion
 
