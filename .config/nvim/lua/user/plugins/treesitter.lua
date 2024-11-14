e15d9583eb493c18636b060cf0da0466b18942d7
ng <ng@noreply.com>
2024-11-14 15:22:51 +0800

    nvim(plugin/treesitter): add ensured_installed array

--- .config/nvim/lua/user/plugins/treesitter.lua
+++ .config/nvim/lua/user/plugins/treesitter.lua
@@ -57,4 +57,11 @@ require('nvim-treesitter.configs').setup {
             },
         },
     },
+    -- Install the parsers for the languages you want to comment in
+    -- Here are the supported languages:
+    ensure_installed = {
+        'astro', 'css', 'glimmer', 'graphql', 'html', 'javascript',
+        'lua', 'nix', 'php', 'python', 'scss', 'svelte', 'tsx', 'twig',
+        'typescript', 'vim', 'vue',
+    },
 }
