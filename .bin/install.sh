7e4e8df0cccfc086a5873a41e010454813e71682
ng <ng@noreply.com>
2025-01-27 16:28:22 +0800

    sh: fix shell script function format
    - use safe shell vars in script

--- .bin/install.sh
+++ .bin/install.sh
@@ -1,27 +1,27 @@
-#!/bin/bash
+#!/bin/sh
 
 if [[ ! -s ~/.dotfiles ]]; then
-    mkdir ~/.dotfiles
+    mkdir "~/.dotfiles"
 fi
 
 # prevent weird git repo recursion problems
-echo ".dotfiles" >> .gitignore
+echo ".dotfiles" >> ".gitignore"
 
-git clone --bare git@github.com:ngtinsmith/dotfiles.git $HOME/.dotfiles
+git clone --bare git@github.com:ngtinsmith/dotfiles.git "$HOME"/.dotfiles
 
-function dotfiles {
-   /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+dotfiles() {
+    /usr/bin/git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME" "$@"
 }
 
-dotfiles checkout
+dotfiles checkout
 
-if [ $? = 0 ]; then
-    echo "Checked out dotfiles config.";
-  else
-    echo "Backing up pre-existing dotfiles.";
-    mkdir -p .dotfiles-backup
-    dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
+if [ $? = 0 ]; then
+    echo "Checked out dotfiles config.";
+else
+    echo "Backing up pre-existing dotfiles.";
+    mkdir -p ".dotfiles-backup"
+    dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
 fi;
 
-dotfiles checkout
-dotfiles config --local status.showUntrackedFiles no
+dotfiles checkout
+dotfiles config --local status.showUntrackedFiles no
