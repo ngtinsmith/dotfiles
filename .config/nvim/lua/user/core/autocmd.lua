b066ed59a365af070ca20c89a346adbf979569b7
ng <ng@noreply.com>
2025-01-27 16:34:51 +0800

    nvim(core): default to `~/.dotfiles` bare git env
    - useful for git plugins like diffview, gitsigns, etc.

--- .config/nvim/lua/user/core/autocmd.lua
+++ .config/nvim/lua/user/core/autocmd.lua
@@ -1,9 +1,10 @@
 local autocmd = vim.api.nvim_create_autocmd
 local augroup = vim.api.nvim_create_augroup
 
--- =============================================================================
-
 local yank = augroup('yank_highlight', { clear = true })
+local lua_modules = augroup('lua_modules', { clear = true })
+local aliases_filetype = augroup('aliases_filetype', { clear = true })
+local git_bare_dots = augroup('git_bare_dots', { clear = true })
 
 autocmd('TextYankPost', {
     group = yank,
@@ -15,10 +16,6 @@ autocmd('TextYankPost', {
     end
 })
 
--- =============================================================================
-
-local lua_modules = augroup('lua_modules', { clear = true })
-
 autocmd('BufWritePost', {
     group = lua_modules,
     desc = 'Source lua files onChange and re-compile packer',
@@ -29,14 +26,29 @@ autocmd('BufWritePost', {
     end,
 })
 
--- =============================================================================
-
-local aliases_filetype = augroup('aliases_filetype', { clear = true })
 
 autocmd({ 'BufRead', 'BufNewFile' }, {
     group = aliases_filetype,
     pattern = '*.aliases',
     callback = function()
         vim.opt.filetype = 'sh'
+    end,
+})
+
+autocmd({ 'BufRead' }, {
+    group = git_bare_dots,
+    desc = [[
+        Set default git work tree and dir to `~/.dotfiles` bare repo.
+        Useful for diffview and gitsigns plugins when working with dotfiles changes
+    ]],
+    pattern = '*.lua',
+    callback = function()
+        local git_work_tree = vim.env.GIT_WORK_TREE
+        local git_dir = vim.env.GIT_DIR
+
+        if git_work_tree == nil and git_dir == nil then
+            vim.env.GIT_WORK_TREE = vim.fn.expand('~')
+            vim.env.GIT_DIR = vim.fn.expand('~/.dotfiles')
+        end
     end,
 })
