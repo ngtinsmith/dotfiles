21a8f436008056c960806e435e7b6dc664f31808
ng <ng@noreply.com>
2025-01-30 22:12:48 +0800

    nvim(chore): remove init git dir and work tree binding
    - it's conditionally done in the autocmd now

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -12,11 +12,3 @@ require('user/core/status')
 
 require('user/projects/keymaps').setup()
 require('user/projects/commands')
-
--- Dotfiles
---
--- used for diffview.nvim
--- TODO: find how to set in bare repo
-
-vim.env.GIT_WORK_TREE = vim.fn.expand('~')
-vim.env.GIT_DIR = vim.fn.expand('~/.dotfiles')
