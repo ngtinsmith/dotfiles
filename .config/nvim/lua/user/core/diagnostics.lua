eacd49ae43d9645f93be32d18fa4b428644d306e
ng <ng@noreply.com>
2025-07-26 20:16:00 +0800

    nvim: enable diagnostics focus

--- .config/nvim/lua/user/core/diagnostics.lua
+++ .config/nvim/lua/user/core/diagnostics.lua
@@ -1,6 +1,6 @@
 vim.diagnostic.config({
     float = {
-        focusable = false,
+        focusable = true,
         prefix = function(diagnostic)
             if diagnostic.code ~= nil then
                 return string.format('(%s) ', type(diagnostic.code) == 'number'
